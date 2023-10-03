c16d0dc8227dfe11ef29bbfa5aed3dd178537220
VelinaChakarov <146725381+VelinaChakarov@users.noreply.github.com>
2023-10-03 14:51:42 +0100

    Add file with test for Summator

---
+++ Summator.Tests/AverageTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Summator.Tests
+{
+    internal class AverageTests
+    {
+        [Test]
+        public void Test_Average_PossitiveNumber()
+        {
+            int result = SumatorProject.Summator.Average(new int[] { 1, 2, 3 });
+            Assert.AreEqual(2, result);
+
+        }
+
+       
+
+    }
+}
+                                                                                
