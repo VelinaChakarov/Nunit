3709c1f11cab0d1a88986ed0eb88835a28cd4110
VelinaChakarov <146725381+VelinaChakarov@users.noreply.github.com>
2023-10-03 14:25:00 +0100

    Commit_Summaror_Project

---
+++ SumatorProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumatorProject
+{
+ public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new long[] { 1, 2, 3, 4, 5, 6,7 });
+            if (result == 21)
+            {
+                Console.WriteLine("Test pass!");
+            }
+            else
+            {
+                Console.WriteLine("Test fail");
+            }
+        }
+    }
+}
---
+++ SumatorProject/Summator.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace SumatorProject
+{
+    public static class Summator
+    {
+        public static long Sum(long[] arr)
+        {
+            long sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            
+            return sum;
+        }
+        public static int Average(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+             
+            }
+            int average = sum / arr.Length;
+
+            return average;
+        }
+    }
+}
